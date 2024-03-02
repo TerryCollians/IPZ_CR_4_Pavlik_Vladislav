31b8ad18291e9bc9eaace6ad4ebd881e3f27169a
Владислав Павлик <144818450+TerryCollians@users.noreply.github.com>
2024-03-02 11:07:12 +0100

    added screen1

--- app/src/main/java/ua/edu/lntu/ipz_cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw4/MainActivity.kt
@@ -3,12 +3,21 @@ package ua.edu.lntu.ipz_cw4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.ipz_cw4.ui.theme.IPZ_CR_4Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +37,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
 
+// Функція першого екрану
+fun Screen1(items: List<String>, onItemClick: (Int) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
+    {
+        Text("Screen 1")
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn{
+
+        }
+
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
